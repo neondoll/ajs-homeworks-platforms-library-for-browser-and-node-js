45ebfcdb8154980a3b48cd9adaed892eb037d234
neondoll <bonni_1998@mail.ru>
2024-03-23 00:26:07 +0300

    second commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function info() {
+  const m = new Map();
+  m.set('name', 'ajs-homeworks-platforms-library-for-browser-and-node-js');
+  m.set('version', '1.0.0');
+  return m;
+}
+
+export default {
+  info
+};
